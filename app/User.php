95c65b1b08c37b7f752e5d50bd2f1adfe4b1cd76
sachi.kkk <k3.git393@gmail.com>
2020-08-08 00:29:45 +0000

    Procfile

--- app/User.php
+++ app/User.php
@@ -120,6 +120,20 @@ class User extends Authenticatable
          return $this->followings()->where('follow_id', $userId)->exists();
      }
      
+     /**
+      * この
+      * ユーザとふぉろ中のユーザの投稿に絞り込む。
+      */
+      public function feed_microposts()
+      {
+          //このユーザがフォロー中のユーザidを取得して配列にする
+          $userIds = $this->followings()->pluck('users.id')->toArray();
+          //このユーザのidもその配列に追加
+          $userIds[] = $this->id;
+          //それらのユーザが所有する投稿に絞り込む
+          return Micropost::whereIn('user_id', $userIds);
+      }
+     
     /**
      * このユーザに関係するモデルの件数をロードする。
      */
